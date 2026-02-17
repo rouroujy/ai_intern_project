7acac4d4f147f159ca7dab16f2d9c71ab79f5c20
rourou <rouroujy5426@163.com>
2026-02-17 17:08:04 +0800

    learn async logging exception!

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,14 +1,35 @@
 import string
 from collections import Counter
 from typing import Dict
+import logging
+import aiofiles
+
+logger = logging.getLogger(__name__)
 
 
-def analyze_text(file_path: str,
+#io错误
+class TextAnalysisError(Exception):
+    """Custom exception for text analysis errors"""
+    pass
+
+
+async def analyze_text(file_path: str,
                  ignore_case: bool = False,
                  remove_punctuation: bool = False) -> Dict:
+    
+    logger.info(f"Start analyzing file: {file_path}")
 
-    with open(file_path, "r", encoding="utf-8") as f:
-        text = f.read()
+    try:
+        async with aiofiles.open(file_path, "r", encoding="utf-8") as f:
+            text = await f.read()
+            logger.debug("Raw text length: %s", len(text))
+
+    except FileNotFoundError:
+        logger.exception("File not found")
+        raise TextAnalysisError("File does not exist")
+    except Exception:
+        logger.exception("Unexpected error while reading file")
+        raise
 
     if ignore_case:
         text = text.lower()
@@ -22,6 +43,8 @@ def analyze_text(file_path: str,
 
     word_counts = Counter(words)
     top_5 = word_counts.most_common(5)
+
+    logger.info("Analysis completed successfully!")
 
     return {
         "lines": len(lines),
--- app/main.py
+++ app/main.py
@@ -1,17 +1,40 @@
 import logging
 import os
 import sys
-from app.analyzer import analyze_text
+from app.analyzer import analyze_text , TextAnalysisError
 import argparse
+import asyncio
+import time
+
+
+logger = logging.getLogger(__name__)
 
 def setup_logging():
         os.makedirs("log",exist_ok=True)
-        logging.basicConfig(
-                level=logging.INFO,
-                filename="log/app.log",
-                filemode="a",
-                format="%(asctime)s - %(levelname)s - %(message)s"
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         )
+        #file handler
+        file_handler = logging.FileHandler("log/app.log")
+        file_handler.setFormatter(formatter)
+
+        #console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+
+        # logging.basicConfig(
+        #         level=logging.INFO,
+        #         filename="log/app.log",
+        #         filemode="a",
+        #         format="%(asctime)s - %(levelname)s - %(message)s"
+        # )
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Text Analyzer Tool")
@@ -21,24 +44,58 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
+async def main():
     print("HOT RELOAD TEST")
-
     args = parse_args()
-    file_path = args.file_path
+
+    # 测试async
+    # start = time.perf_counter()
+
+    # tasks = [
+    #     analyze_text("data/big.txt"),
+    #     analyze_text("data/big.txt"),
+    #     analyze_text("data/big.txt"),
+    # ]
+    # print("测试async")
+    # await asyncio.gather(*tasks)
+
+    # print("Time:",time.perf_counter() - start)
+
     try:
-        result = analyze_text(
-             file_path,
+        result = await analyze_text(
+             args.file_path,
              ignore_case = args.ignore_case,
              remove_punctuation = args.remove_punctuation
         )
-        print("Text Analysis Result:")
+        print("\nText Analysis Result:")
         for key,value in result.items():
              print(f"{key}:{value}")
-        logging.info(f"Successfully analyzed file: {file_path}")
-    except Exception as e:
-        logging.error(f"Error analyzing file: {e}")
-        print(f"Error:{e}")
+
+        
+    #main捕获业务异常
+    except TextAnalysisError as e:
+         logger.error(f"Business error: {e}")
+         print(f"Error: {e}")
+        
+    #系统异常
+    except Exception:
+
+         logger.exception("System level error occurred")
+
+    # file_path = args.file_path
+    # try:
+    #     result = analyze_text(
+    #          file_path,
+    #          ignore_case = args.ignore_case,
+    #          remove_punctuation = args.remove_punctuation
+    #     )
+    #     print("Text Analysis Result:")
+    #     for key,value in result.items():
+    #          print(f"{key}:{value}")
+    #     logging.info(f"Successfully analyzed file: {file_path}")
+    # except Exception as e:
+    #     logging.error(f"Error analyzing file: {e}")
+    #     print(f"Error:{e}")
     
     # print("sys.argv 列表:", sys.argv)
     # if len(sys.argv) < 2 :
@@ -55,4 +112,5 @@ def main():
 
 if __name__=="__main__":
         setup_logging()
-        main()    
+        asyncio.run(main())
+        # main()    
